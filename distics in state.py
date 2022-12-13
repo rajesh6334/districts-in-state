3cbe2de532bb52b366199df0b5132c97a25a3047
rajesh6334 <118992210+rajesh6334@users.noreply.github.com>
2022-12-13 21:53:10 +0530

    Add files via upload

---
+++ distics in state.py
@@ -0,0 +1,12 @@
+#dosticts in state and display in sequential order
+n=int(input('enter no of disticts in state:'))
+districts=[]
+for i in range(n):
+    x=input('entetr the distric name:')
+    districts.append(x)
+print(districts)    
+    
+    
+
+
+      
